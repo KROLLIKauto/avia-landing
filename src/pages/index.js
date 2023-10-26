34ec13b6b657d4a239c3a7d2d36dba30c9f9c320
Nikita <krollikauto94@yandex.ru>
2023-10-26 15:11:24 +0300

    init

---
+++ src/pages/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Header from 'components/header/Header'
+// import Header from '~/components/header/Header'
+// import Header from '../components/header/Header'
+
+export default function index() {
+  return (
+    <div>
+      <div>index</div>
+      <Header></Header>
+    </div>
+  )
+}
